a2745420a5a4a1fb9dfb94f112498402ab056dcb
Steven C Eaton <steveeaton5@gmail.com>
2018-08-13 01:45:33 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -1,7 +1,7 @@
 var playlist = new Object({TheScore: "Legend"});
 
  function updatePlaylist(playlist, artistName, songTitle){
-   playlist.artistName = songTitle
+   playlist.artistName = 'songTitle'
  }
  
 console.log(playlist)
